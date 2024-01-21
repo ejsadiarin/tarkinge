c7a3d7c1225ae8696269b8cad10f2c2dbf7d78eb
ejsadiarin <ejsadiarin@gmail.com>
2024-01-21 18:30:26 +0800

    added todos for go tech to use

--- cmd/main.go
+++ cmd/main.go
@@ -10,6 +10,15 @@ import (
 
 func main() {
 	// TODO: check the chi documentation for important server-side stuff
+	//  --> https://pkg.go.dev/github.com/go-chi/chi/v5@v5.0.11
+	// TODO: use Goth for auth
+	// TODO: use Goose for migrations
+	// TODO: use SQLc for SQL?? or just raw dog SQL (don't use GORM)
+	//  --> see: ~/main/rawdogSQLwithGo.png --> for the SQL
+	// TODO: use Resend for email
+	// TODO: use _ for notifications
+	// TODO: make it PWA
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
